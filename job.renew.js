9ec2494bd04034d87d63242eb46d7b4bdd20ecd8
Kyep <github-aweb@sneakemail.com>
2017-10-22 23:33:31 -0700

    clean up renew.js

--- job.renew.js
+++ job.renew.js
@@ -2,29 +2,26 @@
 
 module.exports = {
 
-    /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.room.name != creep.memory[MEMORY_HOME]) {
             creep.moveTo(creep.getHomePos());
             return 0;
         }
         
-        if(creep.memory[MEMORY_RENEW] == undefined) {
-            // 
-        } else {
+        if(creep.memory[MEMORY_RENEW] != undefined) {
             if(creep.memory[MEMORY_RENEW] == 0) {
                 creep.memory['role'] = 'recycler';
                 return -1;
             }
         }
 
+        creep.say(creep.ticksToLive);
+
         var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_SPAWN && structure.spawning == undefined);
                 }
         });
-        //creep.room.visual.circle(creep.pos, {fill: 'transparent', radius: 0.5, stroke: COLOR_RENEW});
-        creep.say(creep.ticksToLive);
         if(targets.length > 0) {
             var target = creep.pos.findClosestByRange(targets)
             if(target.spawning != null) {
@@ -39,10 +36,13 @@ module.exports = {
                 var body_size = creep.body.length;
                 var renew_cost = Math.ceil(creep_cost/2.5/body_size) * -1;
                 creep.adjustEarnings(renew_cost);
+                if (creep.memory[MEMORY_RENEWALS] == undefined) {
+                    creep.memory[MEMORY_RENEWALS] = 1;
+                } else {
+                    creep.memory[MEMORY_RENEWALS]++;
+                }
             } else if(result == ERR_NOT_IN_RANGE) {
-                // this cumbersome thing stops 8 creeps all trying to renew from surrounding the spawn, which, together with the spawn trying to spawn something, can cause complete lockup.
                 creep.moveTo(target);
-                //creep.moveTo(target, {visualizePathStyle: {stroke: COLOR_RENEW, avoid: [new RoomPosition(target.pos.x - 1, target.pos.y, target.room.name), new RoomPosition(target.pos.x + 1, target.pos.y, target.room.name)]}} );
             } else if (result == ERR_NOT_ENOUGH_ENERGY) {
                 if (creep.carry.energy > 0) {
                     var amount_to_deposit = Math.min(target.energyCapacity - target.energy, creep.carry.energy);
@@ -51,20 +51,11 @@ module.exports = {
                         creep.adjustEarnings(amount_to_deposit);
                     }
                 }
-                return -1;
             } else if (result == ERR_BUSY) {
                 creep.moveTo(target, {visualizePathStyle: {stroke: COLOR_RENEW}});
-                return -1;
-            } else {
-
-                if (creep.memory[MEMORY_RENEWALS] == undefined) {
-                    creep.memory[MEMORY_RENEWALS] = 1;
-                } else {
-                    creep.memory[MEMORY_RENEWALS]++;
-                }
-                
             }
             if (result != OK && result != ERR_NOT_IN_RANGE) { console.log('creep ' + creep.name + ' at ' + creep.pos.x + ',' + creep.pos.y + ':' + creep.room.name + ' failed renew: ' + result) }
+            return result;
         } else {
             //console.log('creep ' + creep.name + ' failed renew (no spawn) ')
             return -1;
