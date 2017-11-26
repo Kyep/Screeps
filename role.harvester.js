4d9a5441ac4443a9b26f34cd58d550d36fbcf820
Kyep <github-aweb@sneakemail.com>
2017-11-25 21:20:38 -0800

    removes containermine from role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -8,7 +8,7 @@ var jobUpgrade = require('job.upgrade');
 var jobRenew = require('job.renew');
 
 module.exports = {
-    run: function(creep, containermine) {
+    run: function(creep) {
         if (creep.memory[MEMORY_DEST] == undefined && creep.memory[MEMORY_SOURCE] != undefined) {
             var source = Game.getObjectById(creep.memory[MEMORY_SOURCE]);
             if(source) {
@@ -60,69 +60,20 @@ module.exports = {
             }
         }
         if (creep.memory[MEMORY_JOB] == JOB_HARVEST) {
-            if (creep.memory[MEMORY_DEST] in Memory.sectors_under_attack && creep.memory[MEMORY_DEST] != creep.memory[MEMORY_HOME] && !containermine) {
+            if (creep.memory[MEMORY_DEST] in Memory.sectors_under_attack && creep.memory[MEMORY_DEST] != creep.memory[MEMORY_HOME]) {
                 creep.memory[MEMORY_JOB] = JOB_TRAVEL_BACK;
                 creep.announceJob();
             } else if (creep.carry.energy == creep.carryCapacity) { // DO NOT DISABLE THIS OR HARVESTERS WILL GET STUCK AND NEVER RETURN!
-                if (containermine) {
-                    var nearby_containers = creep.pos.findInRange(FIND_STRUCTURES, 3, { filter: { structureType: STRUCTURE_CONTAINER } } );
-                    var full_containers = 0;
-                    var thecontainer = undefined;
-                    if (nearby_containers.length > 0) {
-                        for (i = 0; i < nearby_containers.length; i++) {
-                            if(nearby_containers[i].store.energy < nearby_containers[i].storeCapacity) {
-                               thecontainer = nearby_containers[i];
-                               break;
-                            }
-                        }
-                        if(thecontainer == undefined) {
-                            // we can't store resources anywhere - burn some by repairing the containers a lot.
-                            var csites = creep.pos.findInRange(FIND_MY_CONSTRUCTION_SITES, 3);
-                            if (csites.length) {
-                                if(creep.build(csites[0]) == ERR_NOT_IN_RANGE) {
-                                    creep.moveToRUP(csites[0]);
-                                }
-                            } else {
-                                creep.repair(nearby_containers[Math.floor(Math.random() * nearby_containers.length)]);
-                            }
-                        } else {
-                            var result = creep.transfer(thecontainer, RESOURCE_ENERGY)
-                            if (result == ERR_NOT_IN_RANGE) {
-                                creep.moveToRUP(thecontainer);
-                            } else if (result == OK) {
-                                creep.repair(thecontainer);
-                            } else if (result == ERR_FULL) {
-
-                            }
-                        }
-                    } else if (nearby_containers.length == 0) {
-                        // check for construction sites and build one if not.
-                        var csites = creep.pos.findInRange(FIND_MY_CONSTRUCTION_SITES, 3);
-                        if (!csites.length) {
-                            creep.room.createConstructionSite(creep.pos.x, creep.pos.y, STRUCTURE_CONTAINER);
-                        } else {
-                            if(creep.build(csites[0]) == ERR_NOT_IN_RANGE) {
-                                creep.moveToRUP(csites[0]);
-                            }
-                        }
-                    }
-
+                if(creep.room.name == creep.memory[MEMORY_HOME]) {
+                    creep.memory[MEMORY_JOB] = JOB_RETURN;
                 } else {
-                    if(creep.room.name == creep.memory[MEMORY_HOME]) {
-                        creep.memory[MEMORY_JOB] = JOB_RETURN;
-                    } else {
-                        creep.memory[MEMORY_CONTAINER] = undefined;
-                        creep.memory[MEMORY_JOB] = JOB_BUILD;
-                        creep.announceJob();
-                    }
-
+                    creep.memory[MEMORY_CONTAINER] = undefined;
+                    creep.memory[MEMORY_JOB] = JOB_BUILD;
+                    creep.announceJob();
                 }
             } else {
-                if (containermine && !creep.isAtDestination()) {
-                    creep.moveToDestination();
-                }
 	            var retval = jobHarvest.run(creep);
-                if (retval == ERR_NOT_ENOUGH_RESOURCES && !containermine) {
+                if (retval == ERR_NOT_ENOUGH_RESOURCES) {
                     if (creep.carry.energy > 0) {
                         if(creep.room.name == creep.memory[MEMORY_HOME]) {
                             creep.memory[MEMORY_JOB] = JOB_RETURN;
@@ -137,27 +88,16 @@ module.exports = {
         }
         if(creep.memory[MEMORY_JOB] == JOB_BUILD) {
             if (jobBuild.run(creep) == -1) {
-                if (containermine) {
-                    creep.memory[MEMORY_JOB] = JOB_HARVEST;
-                    creep.announceJob();                    
-                } else if(creep.room.name != creep.memory[MEMORY_HOME]) {
+                if (creep.room.name != creep.memory[MEMORY_HOME]) {
                     creep.memory[MEMORY_JOB] = JOB_TRAVEL_BACK;
                     creep.announceJob();
                 } else {
                     if (creep.room.controller) {
                         if (creep.room.controller.level) {
-                            
                             if (creep.room.controller.level < 3) {
                                 creep.memory[MEMORY_JOB] = JOB_REPAIR;
                                 creep.announceJob();
                                 return;
-                            } else if (creep.room.controller.level == 3) {
-                                var towerlist = creep.room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } } );
-                                if (!towerlist.length) {
-                                    creep.memory[MEMORY_JOB] = JOB_REPAIR;
-                                    creep.announceJob();
-                                    return;
-                                }
                             }
                         }
                     }
@@ -197,17 +137,6 @@ module.exports = {
             if (creep.memory[MEMORY_HOME] in Memory.sectors_under_attack) {
                 // don't move towards home while its under attack.
                 return;
-            }
-            if(creep.carry.energy > 0) {
-                var targets = creep.room.find(FIND_STRUCTURES, 3, {
-                    filter: function(structure){
-                        return (structure.hits < structure.hitsMax) && (structure.structureType != STRUCTURE_WALL) && (structure.structureType != STRUCTURE_RAMPART)
-                    }
-                })
-                if(targets.length) {
-                    var target = creep.pos.findClosestByRange(targets)
-                    creep.repair(target);
-                }
             }
             creep.moveToRUP(creep.getHomePos());
             if (creep.room.name == creep.memory[MEMORY_HOME]) {
